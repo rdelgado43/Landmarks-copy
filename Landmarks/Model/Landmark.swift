d5d2c5032e1e09855877d3d1c2aa56b277238c18
Rubiel Delgado <Rdelgado43@cps.edu>
2023-05-22 13:51:45 -0500

    Initial Commit

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,36 @@
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var category: Category
+     enum Category: String, CaseIterable, Codable {
+         case lakes = "Lakes"
+         case rivers = "Rivers"
+         case mountains = "Mountains"
+     }
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    var isFavorite: Bool
+    var isFeatured: Bool
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
